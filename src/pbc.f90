8761fa93060795438705f209e87a863235bd1023
Nsalles <nsalles33@gmail.com>
2022-10-10 16:27:39 +0200

    Complete the Doxygen comment

--- src/pbc.f90
+++ src/pbc.f90
@@ -3,7 +3,6 @@
 !!  Matic Poberjnik,
 !!  Miha Gunde
 
-
 SUBROUTINE pbc( vec, at )
   USE artn_params, ONLY : DP 
   ! 
@@ -36,28 +35,31 @@ SUBROUTINE pbc( vec, at )
   !   
 END SUBROUTINE pbc
 
+
+
 function fpbc( vec, at )RESULT( res )
+  !> @brief 
+  !!   A function that takes into account periodic boundary conditions,
+  !!   based on the pbc function of the contraints_module of QE
+  ! 
   USE artn_params, ONLY : DP
   IMPLICIT none
-  ! 
-  ! A function that takes into account periodic boundary conditions,
-  ! based on the pbc function of the contraints_module of QE
-  ! 
+
   REAL(DP), INTENT(IN)  :: vec(3) ! input vector in atomic units  
   REAL(DP), INTENT(IN)  :: at(3,3)   ! lattice vectors
   REAL(DP)              :: res(3)   ! lattice vectors
-  !REAL(DP), INTENT(IN) :: alat      ! a lattice parameter
+
   REAL(DP)                :: bg(3,3) ! inverse of at(3,3) 
   !
   ! calculate the reciprocal lattice parameters of at 
   ! 
   CALL invmat3x3(at,bg)
   !
-  ! convert to crystal coords 
+  ! convert to crystal coords: U = R.B^-1 
   res(:) = matmul(vec(:),bg(:,:))
   ! move the vector to original box  
   res(:) = res(:) - anint(res(:))
-  ! convert back to cartesian coordinates 
+  ! convert back to cartesian coordinates: R = B.U ??? 
   res(:) = matmul(at(:,:), res(:))
   !   
 END function fpbc
