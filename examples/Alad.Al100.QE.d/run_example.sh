b0d2fcccc5eefc6d5cb491a3ef2cbc1343d6e05b
matic <matic@assam.ijs.si>
2022-10-27 12:03:02 +0200

    changed the run_example.sh
    For the Al adatom diffusion on Al(100); it runs two saddle searches, one
    for hopping, one for exchange

--- examples/Alad.Al100.QE.d/run_example.sh
+++ examples/Alad.Al100.QE.d/run_example.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
-echo "PID",$BASHPID
 #QE_PATH should be defined in environment_variables
 source ../../../environment_variables
 cat > artn.in << EOF 
 &ARTN_PARAMETERS
-  engine_units = 'QE'
-  verbose = 2
-
-  ninit = 3
-  zseed = 96524023
-  init_forc_thr = 1.0d-2
-  forc_thr = 1.0d-3
-  push_step_size = 0.3
-
-! parameters for the push
+  ! parameters for the push
   push_mode = 'list'
   ! define which atoms are to be pushed and the constraints ...
   push_ids = 1, 14
   add_const(:,1) = 1.0, 1.0, -1.0, 0.
   add_const(:,14) = 1.0, 1.0,  1.0, 0.
-
-! lanczos parameters
-  lanczos_disp = 0.1D-1 
-  lanczos_max_size = 16
   ! eigenpush parms
   eigval_thr = -0.005 
-  eigen_step_size = 0.3
   lpush_final = .true.
 /
 EOF
 
 mpirun -np 16 ${QE_PATH}/bin/pw.x -partn  < artn.Al-hollow.Al100-5x5-6l.in > artn.Al-hollow.Al100-5x5-6l.out  
+
+cp artn.out artn-exchange.out
+
+cat > artn.in << EOF 
+&ARTN_PARAMETERS
+  ! parameters for the push
+  push_mode = 'list'
+  ! define which atoms are to be pushed and the constraints ...
+  push_ids = 1
+  add_const(:,1) = 1.0, 1.0, -1.0, 0.
+  ! eigenpush parms
+  lpush_final = .true.
+/
+EOF
+
+mpirun -np 16 ${QE_PATH}/bin/pw.x -partn  < artn.Al-hollow.Al100-5x5-6l.in > artn.Al-hollow.Al100-5x5-6l.out  
+
+cp artn.out artn-hopping.out
 
