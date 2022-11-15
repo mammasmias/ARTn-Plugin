e4534237b84a47cce39f83ec32a8aebfc2c46439
Matic <matic@beast.cm.sissa.it>
2022-11-15 12:19:58 +0100

    Fixed graphene vacancy example and added the reference

--- examples/graphene.QE.d/run_example.sh
+++ examples/graphene.QE.d/run_example.sh
@@ -3,4 +3,4 @@
 #QE_PATH should be define in environment_variables
 source ../../environment_variables
 
-mpirun -np 4 ${QE_PATH}/bin/pw.x -partn < relax.graphene-3x2.C-vac.in | tee relax.graphene-3x2.C-vac.out
+${PARA_PREFIX} ${QE_PATH}/bin/pw.x -partn < relax.graphene-3x2.C-vac.in | tee relax.graphene-3x2.C-vac.out
