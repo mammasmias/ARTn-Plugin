79dea2928621bf174e03f82942b92d0eb5c4a3e9
marsamos <lmartinsamos@gmail.com>
2022-12-15 12:25:49 +0100

    update run example Al

--- examples/Al-vacancy.d/run_example.sh
+++ examples/Al-vacancy.d/run_example.sh
@@ -3,4 +3,4 @@
 #QE_PATH should be define in environment_variables
 source ../../environment_variables
 
-mpirun -np 2 ${QE_PATH}/bin/pw.x -partn < relax.Al-vacancy.in | tee relax.Al-vacancy.out
+$(PARA_PREFIX) ${QE_PATH}/bin/pw.x -partn < relax.Al-vacancy.in | tee relax.Al-vacancy.out
